1c0c4f5b35decaf720db14b838c0798023113518
unknown <diego.alvarado@ucb.edu.bo>
2024-03-16 17:51:37 -0400

    navbar1

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<nav ref={navRef}>
+				<a href="/#">Home</a>
+				<a href="/#">Servicios</a>
+				<a href="/#">Doctores</a>
+				<a href="/#">Información</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+	);
+}
+
+export default Navbar;
