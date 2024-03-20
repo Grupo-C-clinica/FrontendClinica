79a9a05d2939774e1755bfe5e923db7f05781657
615Alberto615 <a.encinas115@gmail.com>
2024-03-20 15:19:23 -0400

    info

---
+++ src/components/Informacion.jsx
@@ -0,0 +1,23 @@
+//32:08
+const Información = () => {
+    return (
+        <div className="my-24 md:px-14 px-4 max-w-screen-2xl mx-auto">
+            <div className="flex flex-col lg:flex-row justify-between items-center gap-10">
+                <div className="lg:w-1/4">
+                    <h3 className="text-3xl text-primary font-bold lg:w-1/2 mb-3">Why we are better than others  </h3>
+                    <p className="text-base text-tartiary">A simple paragraph is comprised of three major components. The first sentence, which is often a declarative sentence, is called the “topic sentence.”</p>
+                </div>
+
+                <div className="w-full lg:w-3/4">
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Información;
