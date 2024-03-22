65834544ea056419a7dd532a3dc967f06d1f3aea
615Alberto615 <a.encinas115@gmail.com>
2024-03-22 18:24:50 -0400

    arr3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx}", // Asegúrate de incluir la ruta correcta para tus archivos React
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#010851",
+        "secondary": "#9A7AF1",
+        "tartary": "#707070",
+        "pink": "#EE9AE5",
+      },
+      boxShadow: {
+        "3xl":'0 10px 50px 0px rgba(0,0,0,0.15)',
+      },
+      fontFamily: {
+        
+      },
+    },
   },
   plugins: [],
 }
