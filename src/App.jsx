e568028b692f8765736497e866f1a32aaabca5d7
unknown <diego.alvarado@ucb.edu.bo>
2024-03-23 11:04:05 -0400

    Ajustes Contactos (tailwind.css)

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,26 @@ import {
   Link
 } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/HomeA";
+import Informacion from "./components/Informacion";
+import Footer from "./components/Footer/Footer";
+import Contacts from "./components/Contacts/contacts";
+import Wrapper from "./components/ContenedorGlobal/Contenedor";
+
 const App = () => {
   return (
     <>
     
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      
+    <Navbar/>
+    <Wrapper id="contacts" heading="Contactos">
+      <Contacts />
+    </Wrapper>
+      <Informacion></Informacion>
+    <Footer/>
     </>
   );
 };
 
 export default App;
 
-
-/*
-<Navbar/>
-    <Home/>
-    <Informacion></Informacion>
-*/
