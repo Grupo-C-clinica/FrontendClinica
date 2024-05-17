2326b553d6ec7e04980c0fadc11fab1e8e291426
615Alberto615 <a.encinas115@gmail.com>
2024-05-17 19:35:22 -0400

    ultimo ghpages

--- src/components/logeado/Navbarlogeado.jsx
+++ src/components/logeado/Navbarlogeado.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logo from '../../assets/logo.png';
+
+//iconos
 import { FaTimes, FaBars, FaUserCircle } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // Obtener los detalles del usuario al montar el componente
+  useEffect(() => {
+    
+  }, []);
+
+
   const togglerMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  };
-
+  }
   const navItems = [
     { link: "Registrar nuevo paciente", path: "/FrontendClinica/registroPacientes" },
     { link: "Lista Pacientes", path: "/FrontendClinica/pacientes" },
@@ -20,8 +27,8 @@ const Navbar = () => {
   const toggleProfileMenu = () => {
     setIsProfileMenuOpen(!isProfileMenuOpen);
   };
-
   const handleLogout = () => {
+    
     window.location.href = `/`; // O redireccionar a la página que consideres adecuada
   };
 
@@ -30,22 +37,24 @@ const Navbar = () => {
       <nav className='bg-white md:px-14 p-4 max-w-screen-2xl border-b mx-auto text-primary fixed top-0 right-0 left-0 z-10'>
         <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
           <div className='flex space-x-14 items-center'>
-            <Link to="/" className='text-2xl font-semibold flex items-center space-x-3 text-primary'>
+            <a href="/" className='text-2xl font-semibold flex items-center space-x-3 text-primary'>
               <img src={logo} alt="UCB Logo" className='w-10 inline-block items-center'/>
               <span>UCB</span>
-            </Link>
+            </a>
             <ul className="md:flex space-x-12 hidden">
-              {navItems.map(({ link, path }) => (
-                <Link to={path} key={link} className='block hover:text-gray-300 cursor-pointer'>{link}</Link>
+              {navItems.map(({link, path}) => (
+                <a href={path} key={link} className='block hover:text-gray-300 cursor-pointer'>{link}</a>
               ))}
             </ul>
           </div>
           <div className='hidden md:block relative'>
             <button onClick={toggleProfileMenu} className='flex items-center focus:outline-none'>
+             
               <FaUserCircle className='w-8 h-8 text-primary' />
             </button>
             {isProfileMenuOpen && (
               <div className='absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20'>
+                
                 <button onClick={handleLogout} className='block px-4 py-2 text-sm text-primary hover:bg-gray-100 w-full text-left'>Cerrar sesión</button>
               </div>
             )}
@@ -61,17 +70,18 @@ const Navbar = () => {
         <button onClick={togglerMenu} className='text-primary mb-8'>
           <FaTimes className='w-8 h-8'/>
         </button>
-        {navItems.map(({ link, path }) => (
-          <Link to={path} key={link} className='block hover:text-gray-300 text-primary mb-4' onClick={togglerMenu}>{link}</Link>
+        
+        {navItems.map(({link, path}) => (
+          <a href={path} key={link} className='block hover:text-gray-300 text-primary mb-4' onClick={togglerMenu}>{link}</a>
         ))}
-        <div>
-          <Link to="/historial" className='text-primary hover:bg-gray-100 display-block'>Historial</Link>
+        <div className=''>
+          <a href="/historial" className='text-primary hover:bg-gray-100 display-block'>Historial</a>
           <button onClick={handleLogout} className='text-primary hover:bg-gray-100 display-block w-full text-left mt-2'>Cerrar sesión</button>
         </div>
       </div>
     </>
   );
-};
+}
 
 
 export default Navbar;
