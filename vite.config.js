2ec23944d9d80240fa9bee7640263b1b07ece5ee
615Alberto615 <a.encinas115@gmail.com>
2024-05-17 16:23:15 -0400

    arreglos

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  /*base: "/FrontendClinica/",*/
+  base: '/FrontendClinica/', // Cambia esto a tu repositorio
   plugins: [react()],
-})
-
+  build: {
+    outDir: 'docs' // Cambia esto si tu directorio de salida es diferente
+  }
+});
