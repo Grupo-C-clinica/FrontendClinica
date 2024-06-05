986437f37646b688923219d5f497404dbb69e416
615Alberto615 <a.encinas115@gmail.com>
2024-06-04 23:15:36 -0400

    login arreglos y cerrarsion PA arreglado

--- src/components/admin/navbarAdmin.jsx
+++ src/components/admin/navbarAdmin.jsx
@@ -1,45 +1,41 @@
 import { useState, useEffect } from 'react';
 import logo from '../../assets/logo.png';
 import useAuthStore from '../../store/useAuthStore'; 
-//iconos
 import { FaTimes, FaBars, FaUserCircle } from 'react-icons/fa';
 
 const Navbar = () => {
-  
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
   const logout = useAuthStore((state) => state.logout); 
 
-  // Obtener los detalles del usuario al montar el componente
-  useEffect(() => {
-    
-  }, []);
-
+  useEffect(() => {}, []);
 
   const togglerMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-  }
-  const navItems = [
-    {/*
-        { link: "Registrar nuevo paciente", path: "/FrontendClinica/registroPacientes" },
-    { link: "Lista Pacientes", path: "/FrontendClinica/pacientes" },
-    { link: "Citas", path: "/FrontendClinica/citas" },
+  };
 
-  
-  */ },
+  const navItems = [
     {link: "Registrar nuevo Usuario", path: "/registroUsuarios"},
     {link: "Lista de Usuarios", path: "/listaUsuarios"},
-    
   ];
 
   const toggleProfileMenu = () => {
     setIsProfileMenuOpen(!isProfileMenuOpen);
   };
+
   const handleLogout = () => {
     logout(); // Llama a la función logout
     window.location.href = '/'; // Redirige a la página principal
   };
 
+  const openLogoutModal = () => {
+    setIsLogoutModalOpen(true);
+  };
+
+  const closeLogoutModal = () => {
+    setIsLogoutModalOpen(false);
+  };
 
   return (
     <>
@@ -58,13 +54,11 @@ const Navbar = () => {
           </div>
           <div className='hidden md:block relative'>
             <button onClick={toggleProfileMenu} className='flex items-center focus:outline-none'>
-             
               <FaUserCircle className='w-8 h-8 text-primary' />
             </button>
             {isProfileMenuOpen && (
               <div className='absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20'>
-                
-                <button onClick={handleLogout} className='block px-4 py-2 text-sm text-primary hover:bg-gray-100 w-full text-left'>Cerrar sesión</button>
+                <button onClick={openLogoutModal} className='block px-4 py-2 text-sm text-primary hover:bg-gray-100 w-full text-left'>Cerrar sesión</button>
               </div>
             )}
           </div>
@@ -79,18 +73,34 @@ const Navbar = () => {
         <button onClick={togglerMenu} className='text-primary mb-8'>
           <FaTimes className='w-8 h-8'/>
         </button>
-        
         {navItems.map(({link, path}) => (
           <a href={path} key={link} className='block hover:text-gray-300 text-primary mb-4' onClick={togglerMenu}>{link}</a>
         ))}
         <div className=''>
           <a href="/historial" className='text-primary hover:bg-gray-100 display-block'>Historial</a>
-          <button onClick={handleLogout} className='text-primary hover:bg-gray-100 display-block w-full text-left mt-2'>Cerrar sesión</button>
+          <button onClick={openLogoutModal} className='text-primary hover:bg-gray-100 display-block w-full text-left mt-2'>Cerrar sesión</button>
         </div>
       </div>
+
+      {isLogoutModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+          <div className="bg-white rounded-lg p-6 w-1/3 mx-auto relative text-center">
+            <h3 className="text-lg font-bold mb-4">Confirmar Cierre de Sesión</h3>
+            <p>¿Estás seguro de que quieres cerrar sesión?</p>
+            <div className="mt-4 flex justify-around">
+              <button onClick={closeLogoutModal} className="bg-gray-300 text-gray-700 px-4 py-2 rounded hover:bg-gray-400 transition-colors duration-300">
+                Cancelar
+              </button>
+              <button onClick={handleLogout} className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors duration-300">
+                Cerrar Sesión
+              </button>
+            </div>
+            <button className="absolute top-2 right-2 text-2xl font-bold" onClick={closeLogoutModal}>&times;</button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
-
 
 export default Navbar;
