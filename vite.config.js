bb96f237783024c06fdbb9679bea7e570db5613f
615Alberto615 <a.encinas115@gmail.com>
2024-06-05 16:35:37 -0400

    deploy ger

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/FrontendClinica/', // Asegúrate de que esto apunte a tu repositorio
+  base: '/', // Asegúrate de que esto apunte a tu repositorio
   plugins: [react()],
   build: {
     outDir: 'dist' // Cambia esto si tu directorio de salida es diferente
